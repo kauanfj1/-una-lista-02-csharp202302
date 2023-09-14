70a2d0f4dc45315b90cc5409f4aa20520cd1d6d1
kauanfj1 <143045542+kauanfj1@users.noreply.github.com>
2023-09-14 14:57:14 -0300

    Add files via upload

---
+++ Atividade 01.cs
@@ -0,0 +1,18 @@
+
+class Atividade02_01
+{
+    
+    
+        int num1, num2, media;
+
+        Console.WriteLine("Digite o primeiro numero: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo numero: ");
+        num2 = int.Parse(Console.ReadLine());
+
+        media = (num1 + num2) / 2;
+
+        Console.WriteLine("A média é: " + media);
+    
+}
